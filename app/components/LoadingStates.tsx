9263f442fb3d9139a7fcba77c6e8d73ad9293f0f
Ryan Sutton <ryan@pixieset.com>
2025-07-17 14:31:03 -0700

    Updating the creating your storyboard page

--- app/components/LoadingStates.tsx
+++ app/components/LoadingStates.tsx
@@ -6,18 +6,18 @@ export function LoadingPipeline() {
       <div className="fixed inset-0 bg-[#e1f2ec]" style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif' }}>
         {/* Centered loading text and loader */}
         <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <p className="text-[#343434] text-[25px] leading-[32px] mb-8">
-            Creating your<br />storyboard...
-          </p>
-          
           {/* Square Loader */}
-          <div className="square-loader">
+          <div className="square-loader mb-8">
             <span className="loader">
               <span className="loader-inner"></span>
             </span>
           </div>
+
+          <p className="text-[#343434] text-[25px] leading-[32px]">
+            PixieDirector is creating<br />your storyboard...
+          </p>
         </div>
-        
+
         {/* Home Indicator */}
         <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2">
           <div className="w-36 h-[5px] bg-black rounded-full" />
@@ -51,19 +51,19 @@ export function LoadingPipeline() {
             0% {
               transform: rotate(0deg);
             }
-            
+
             25% {
               transform: rotate(180deg);
             }
-            
+
             50% {
               transform: rotate(180deg);
             }
-            
+
             75% {
               transform: rotate(360deg);
             }
-            
+
             100% {
               transform: rotate(360deg);
             }
@@ -73,19 +73,19 @@ export function LoadingPipeline() {
             0% {
               height: 0%;
             }
-            
+
             25% {
               height: 0%;
             }
-            
+
             50% {
               height: 100%;
             }
-            
+
             75% {
               height: 100%;
             }
-            
+
             100% {
               height: 0%;
             }
