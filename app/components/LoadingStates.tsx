1da53821538c42d389c4fe31d2b587c77d5b36f0
Ryan Sutton <ryan@pixieset.com>
2025-07-17 14:55:55 -0700

    Updating the creating storyboard page

--- app/components/LoadingStates.tsx
+++ app/components/LoadingStates.tsx
@@ -5,7 +5,7 @@ export function LoadingPipeline() {
     return (
       <div className="fixed inset-0 bg-[#e1f2ec]" style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif' }}>
         {/* Centered loading text and loader */}
-        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-80 max-w-[90vw]">
           {/* Square Loader */}
           <div className="square-loader mb-8">
             <span className="loader">
@@ -16,11 +16,6 @@ export function LoadingPipeline() {
           <p className="text-[#343434] text-[25px] leading-[32px]">
             PixieDirector is creating<br />your storyboard...
           </p>
-        </div>
-
-        {/* Home Indicator */}
-        <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2">
-          <div className="w-36 h-[5px] bg-black rounded-full" />
         </div>
 
         <style jsx>{`
